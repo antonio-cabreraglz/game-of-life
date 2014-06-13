2f038eec51ddd0537a84bad908d3dba62187858a
Antonio Cabrera <antonio.cabreraglz@gmail.com>
2014-06-13 12:23:20 -0500

    Draw Cells

--- gosu_gol.rb
+++ gosu_gol.rb
@@ -11,9 +11,12 @@ class GameWindow < Gosu::Window
     super width, heigth, false
     self.caption = "Game of Life"
     @background_color = Gosu::Color.new(0xffdedede)
+    @cell_color = Gosu::Color.new(0xff000000)
 
     @columns = width/10
+    @column_width = @columns
     @rows = heigth/10
+    @row_heigth = heigth / @rows
     @board = Board.new(@rows,@columns)
     @game = Game.new(@board)
     @game.board.randomly_populate!
@@ -30,6 +33,11 @@ class GameWindow < Gosu::Window
               width, @heigth, @background_color,
               0, @heigth, @background_color)
     @game.board.cells.each_value do |cell|
+      color = (@cell_color if cell.is_alive?) || @background_color
+      draw_quad(cell.x * @column_width, cell.y * @row_heigth,color,
+                cell.x * @column_width + @column_width, cell.y * @row_heigth,color,
+                cell.x * @column_width + @column_width, cell.y * @row_heigth + @row_heigth, color,
+                cell.x * @column_width, cell.y * @row_heigth + @row_heigth, color)
 
     end
   end
